b8a94045d5a27d0670a593ecde5d8771a3c204b8
Rainiersoft101 <Rainiersoft101@192.168.0.115>
2019-10-01 18:31:52 +0530

    replace files

--- pack/src/test/java/CommonFunctionLibrary/FunctionLibrary.java
+++ pack/src/test/java/CommonFunctionLibrary/FunctionLibrary.java
@@ -236,7 +236,7 @@ public class FunctionLibrary {
 	
 	{
 		Thread.sleep(3000);
-		 Runtime.getRuntime().exec("C:\\Users\\Rainiersoft101\\Desktop\\resume5.exe");
+		 Runtime.getRuntime().exec("E:\\Resume - Copy.exe");
 	}
 	
 	
